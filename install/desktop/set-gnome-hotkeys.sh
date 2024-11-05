4e9bc2bac685ab86e662b1085fcdeddb5e6588b1
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2024-11-05 12:28:41 -0300

    feat: add set-gnome-hootkeys

---
+++ install/desktop/set-gnome-hotkeys.sh
@@ -0,0 +1,6 @@
+# close window
+gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4']"
+
+# show desktop = minimize all
+gsettings set org.gnome.desktop.wm.keybindings show-desktop  "['<Super>D']"
+
