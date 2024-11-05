58327e556e04d7d867c781ffd1d5232de759aba2
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2024-11-05 12:40:11 -0300

    feat: add custom shortcut for kitty terminal

--- install/desktop/set-gnome-hotkeys.sh
+++ install/desktop/set-gnome-hotkeys.sh
@@ -4,3 +4,10 @@ gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4']"
 # show desktop = minimize all
 gsettings set org.gnome.desktop.wm.keybindings show-desktop  "['<Super>D']"
 
+# Reserve slots for custom keybindings
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Open Kitty
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'kitty'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'kitty'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>t'
