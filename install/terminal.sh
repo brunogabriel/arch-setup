da797bde54fa199120b3d445bc63053e596d35eb
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2025-10-12 09:00:30 -0300

    feat: add opencode

--- install/terminal.sh
+++ install/terminal.sh
@@ -6,11 +6,7 @@ sudo pacman -S curl git unzip --noconfirm
 sudo pacman -S --needed base-devel --noconfirm
 
 # yay
-if ! command -v yay >/dev/null 2>&1; then
-  git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
-  cd .. && rm -rf yay
-  yay -Syu --noconfirm
-fi
+sudo pacman -S --needed yay --noconfirm
 
 # terminal tools
 for installer in ./install/terminal/*.sh; do source $installer; done
