9ae3efe64ae43b446ade9148df36fcf869d0036d
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2025-11-16 16:16:25 -0300

    chore: add docker support to firewall

--- install/desktop/gufw.sh
+++ install/desktop/gufw.sh
@@ -1,9 +1,20 @@
 yay -S --needed gufw --noconfirm
+yay -S --needed ufw-docker --noconfirm
 
 gum style --foreground 212 "Confuguring UFW firewall default rules..."
 
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
+# Allow Docker containers to use DNS on host
+sudo ufw allow in proto udp from 172.16.0.0/12 to 172.17.0.1 port 53 comment 'allow-docker-dns'
+
+# Turn on the firewall
 sudo ufw --force enable
+
+# Enable UFW systemd service to start on boot
 sudo systemctl enable ufw
+
+# Turn on Docker protections
+sudo ufw-docker install
+sudo ufw reload
