808869cccb5361ef618be562e7b512ddfd0cfd76
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2025-11-16 10:13:49 -0300

    feat: add firewall as a system service after install

--- install/desktop/gufw.sh
+++ install/desktop/gufw.sh
@@ -1,1 +1,9 @@
 yay -S --needed gufw --noconfirm
+
+echo "Configuring UFW firewall default rules..."
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw --force enable
+sudo systemctl enable ufw
